bb96aaad5203ba13f9466cb0f6e2ae35b5a244f4
Kenneth <kenny.laprelle@gmail.com>
2019-07-11 15:16:49 -0700

    new

--- client/src/sitemap-builder.js
+++ client/src/sitemap-builder.js
@@ -1,10 +1,10 @@
-require('babel-register');
+// require('babel-register');
 
-const router = require('./index').default;
-const Sitemap = require('../lib').default;
+// const router = require('./index').default;
+// const Sitemap = require('../lib').default;
 
-(
-  new Sitemap(router)
-    .build('http://strongandhayden.com', { limitCountPaths: 5000 })
-    .save('./sitemap.xml', '/static/')
-);
+// (
+//   new Sitemap(router)
+//     .build('http://strongandhayden.com', { limitCountPaths: 5000 })
+//     .save('./sitemap.xml', '/static/')
+// );
