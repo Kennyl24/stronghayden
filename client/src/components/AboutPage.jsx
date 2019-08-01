7f55008de7179c3ee73d5b02a522608ef915ebf8
Kenneth <kenny.laprelle@gmail.com>
2019-08-01 09:59:26 -0700

    new about page

--- client/src/components/AboutPage.jsx
+++ client/src/components/AboutPage.jsx
@@ -39,7 +39,7 @@ class AboutPage extends React.Component {
       {/* <h1 style={{fontWeight:'800', color:'#3d633b', fontSize:'40px', textAlign:'center'}}>About Strong & Hayden</h1> */}
       {/* <TeamExperience/> */}
       <AboutUs/>
-      <div style={{height:'250px', backgroundColor:'#eee', color:'black'}}> 
+      <div style={{height: isMobile ? '450px': '250px', backgroundColor:'#eee', color:'black'}}> 
       <h1 style={{textAlign:'center', margin:0, lineHeight:'40px', color:'green'}}> 
       Reviews
       </h1>
@@ -49,9 +49,9 @@ class AboutPage extends React.Component {
       </h1>
         </div>
   
-      <div className="home-background" style={{position:'relative', top: isMobile ? '120px':'00px',backgroundColor: 'rgba(238, 238, 238, 0.41) !important', backgroundImage: `url(${aboutsec})`, backgroundRepeat: 'no-repeat', imageRendering: '-webkit-optimize-contrast', backgroundSize:isMobile ? 'contain':'cover', height: isMobile ? '350px' : '350px', width: '100%', marginBottom:'0px'}}>
+      <div className="home-background" style={{position:'relative', top: isMobile ? '00px':'00px',backgroundColor: 'rgba(238, 238, 238, 0.41) !important', backgroundImage: isMobile ? null: `url(${aboutsec})`, backgroundRepeat: 'no-repeat', imageRendering: '-webkit-optimize-contrast', backgroundSize:isMobile ? 'contain':'cover', height: isMobile ? '550px' : '350px', width: '100%', marginBottom:'0px'}}>
       <div className="scrim2">
-      <p className= {isMobile ? 'about-us-mobile2' :"about-us2"}>
+      <p className= {isMobile ? 'about-us-mobile2' :"about-us2"} style={{lineHeight: isMobile ? '25px': '35px'}}>
       Strong & Hayden Commercial Real Estate provides tenants, owners and investors with a broad range of real estate services in Napa and Solano Counties. With an experienced team of professionals who have sold and leased over 2,800,000 square feet in commercial transactions, the company’s extensive local expertise generates a product that is best for the client and sustainable for the community.
       Our team and industry relationships allow us to develop a truly value added experience for our clients. Innovative, aggressive, and progressive, the  Strong & Hayden Real Estate generates a product that is best for the client  and sustainable for the community. Put our extensive network to work for you.</p>
       </div>
--- client/src/components/AboutUs.jsx
+++ client/src/components/AboutUs.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Link } from 'react-router-dom';
+import {isMobile} from 'react-device-detect';
 
 
 class AboutUs extends React.Component { 
@@ -15,19 +16,19 @@ class AboutUs extends React.Component {
       <div style={{marginTop:'30px'}}>
       {/* <h1 style={{color:'rgb(62, 98, 58)', textAlign:'center', marginTop:'20px'}}>Our Broker's Offer</h1> */}
       <div style={{display:'flex', flexDirection:'row', flexWrap:'wrap', alignItems:'center', marginLeft:'10%', marginRight:'10%'}}>
-      <div className="about-square">
+      <div className="about-square" style={{height: isMobile ? 'auto' : '300px'}}>
       <h1 className="offer-header"> Local Expertiese</h1>
       <p>  A staff of professional brokers and clerical support personnel, dealing exclusively with commercial properties </p>
       </div>
-        <div className="about-square">
+        <div className="about-square" style={{height: isMobile ? 'auto' : '300px'}}>
         <h1 className="offer-header"> Team Concept</h1>
         <p>  The synergy of qualified brokers working cooperatively in the development and implementation of your marketing plan, and to work with the local and state government agencies </p>
         </div>
-        <div className="about-square">
+        <div className="about-square" style={{height: isMobile ? 'auto' : '300px'}}>
         <h1 className="offer-header"> Advantage To You</h1>
        <p> We know the market! Our database of leasing and sales information helps us provide you with comprehensive knowledge of the market. </p>
         </div>
-        <div className="about-square">
+        <div className="about-square" style={{height: isMobile ? 'auto' : '300px'}}>
         <h1 className="offer-header"> Results Driven</h1>
         <p>We can offer savings in both time and money. We can provide an increased number of prospective tenants. We can also give you maximum exposure of your property if you list with us. </p>
         </div>
