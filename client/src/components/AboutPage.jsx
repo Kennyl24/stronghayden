6a7995bd11180ba5f91d7db0e0bb371d6aa9f189
Kenneth <kenny.laprelle@gmail.com>
2019-08-01 16:15:44 -0700

    new review

--- client/src/components/AboutPage.jsx
+++ client/src/components/AboutPage.jsx
@@ -20,10 +20,28 @@ class AboutPage extends React.Component {
     super(props);
     this.state = {
       showIcon: false,
+      odd: false
     }
   }
   componentDidMount() {
+  let i = 0;
+  setInterval(function() {
+
+      if (i < 50) {
+        this.setState({
+          odd: !this.state.odd,
+        });
+      }
+      else return;
+      i++;
+  }.bind(this), 7000);
+  //   setTimeout(() => {
+  //     this.setState({
+  //       odd: !this.state.odd,
+  //     })
+  // }, 5000);
     window.scrollTo(0, 0);
+
 }
   render () {
     return (
@@ -39,15 +57,34 @@ class AboutPage extends React.Component {
       {/* <h1 style={{fontWeight:'800', color:'#3d633b', fontSize:'40px', textAlign:'center'}}>About Strong & Hayden</h1> */}
       {/* <TeamExperience/> */}
       <AboutUs/>
-      <div style={{height: isMobile ? '450px': '250px', backgroundColor:'#eee', color:'black'}}> 
+      <div style={{height: isMobile ? '450px': '300px', backgroundColor:'#eee', color:'black'}}> 
       <h1 style={{textAlign:'center', margin:0, lineHeight:'40px', color:'green'}}> 
       Reviews
       </h1>
+      {this.state.odd ? 
+      <div className="serviceicon-two">
         <p className={isMobile ? 'about-us-mobile' :"about-us"}>"We have been looking for a space for a wine bar/tasting room in Napa. This is a tall order as the market is so competitive and limited. I feel very fortunate in finding this company. Since day one my realtor has been nothing other than responsive, professional, accommodating, and utterly knowledgeable about this market."</p>
         <h1 style={{textAlign:'center', marginTop:'20px', fontSize:'16px', lineHeight:'40px', color:'green'}}> 
       -Thomas M.
       </h1>
-        </div>
+      <div style={{textAlign:'center'}}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><circle fill="white" cx="12" cy="12" r="8"/></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><circle fill="#010101" cx="12" cy="12" r="8"/></svg>
+      </div>
+      </div>
+      :
+      <div className="serviceicon-two">
+      <p className={isMobile ? 'about-us-mobile' :"about-us"}>"I called Strong & Hayden on a friend's recommendation when I found out I had one month to move my business.  They got back to me immediately and within 48 hours I was shaking hands with a realtor and submitting an offer on the perfect office space.  They have a real knowledge of what is available in the Napa community and are very efficient in connecting business owners to the space that will best suit their needs."</p>
+      <h1 style={{textAlign:'center', marginTop:'20px', fontSize:'16px', lineHeight:'40px', color:'green'}}> 
+    -Trisha T.
+    </h1>
+    <div style={{textAlign:'center'}}>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><circle fill="#010101" cx="12" cy="12" r="8"/></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><circle fill="white" cx="12" cy="12" r="8"/></svg>
+      </div>
+    </div>}
+    
+    </div>
   
       <div className="home-background" style={{position:'relative', top: isMobile ? '00px':'00px',backgroundColor: 'rgba(238, 238, 238, 0.41) !important', backgroundImage: isMobile ? null: `url(${aboutsec})`, backgroundRepeat: 'no-repeat', imageRendering: '-webkit-optimize-contrast', backgroundSize:isMobile ? 'contain':'cover', height: isMobile ? '550px' : '350px', width: '100%', marginBottom:'0px'}}>
       <div className="scrim2">
